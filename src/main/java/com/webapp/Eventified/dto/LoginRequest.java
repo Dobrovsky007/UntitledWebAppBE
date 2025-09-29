2f4bb1630b09ed47af9ba0d3b7df0f79ffdf0136
“[matus <tkac@provys.com>
2025-09-29 10:50:17 +0200

    Added JUnit dependancy to pom.xml, added constructor annotation to LoginRequest.java and created UnitTests for AuthServiceTest.java

--- src/main/java/com/webapp/Eventified/dto/LoginRequest.java
+++ src/main/java/com/webapp/Eventified/dto/LoginRequest.java
@@ -1,9 +1,13 @@
 package com.webapp.Eventified.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
+
 }
